19399931a57e7dae7e6dc746baaed7f1c4c58d72
sh-islam <misla2@uwo.ca>
2024-05-28 00:57:46 -0400

    Lottie anim and styling complete

--- src/footer.js
+++ src/footer.js
@@ -1,14 +1,25 @@
 // footer.js
-import React from "react";
+import React, {useRef} from "react";
 import "./footer.css";
 import Lottie from "lottie-react";
 import githubIcon from "./icons8-github.json"; 
 
 function Footer() {
+  const githubIconRef = useRef();
+
   return (
     <div className="footer">
       <div className="footer-container">
-        <Lottie animationData={githubIcon} className="gh-icon-anim" />
+        <a href="https://github.com/sh-islam/excel-search" target="_blank" rel="noreferrer">
+          <Lottie 
+            animationData={githubIcon}
+            lottieRef={githubIconRef} 
+            className="gh-icon-anim"
+            autoplay ={false}
+            loop = {1}  // Does not autoplay, but loops once on hover/pointer enter
+            onPointerEnter={()=>{githubIconRef.current.goToAndPlay(1,true);}}
+          />
+        </a>
         <a href="mailto:misla2@uwo.ca" className="contact-link">
           Contact
         </a>
