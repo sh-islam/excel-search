1cf727057bcba3b300a107365278da59017d4d98
sh-islam <misla2@uwo.ca>
2024-05-28 00:25:35 -0400

    footer and header fixed

--- src/footer.js
+++ src/footer.js
@@ -1,15 +1,14 @@
+// footer.js
 import React from "react";
 import "./footer.css";
-import appIcon from "./app-icon.png";
-import { Lottie } from "lottie-react";
+import Lottie from "lottie-react";
 import githubIcon from "./icons8-github.json"; 
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer-container">
-      <img src={appIcon} className="gh-icon" alt="App Icon" />
-        {/* <Lottie animationData={githubIcon} className="logo-animation" /> */}
+        <Lottie animationData={githubIcon} className="gh-icon-anim" />
         <a href="mailto:misla2@uwo.ca" className="contact-link">
           Contact
         </a>
